719da3057161f4578f618880491fd31a8f463093
didipta <66532706+didipta@users.noreply.github.com>
2022-09-05 21:31:38 +0600

    Initial commit

---
+++ infotect_task/resources/views/car/carlist.blade.php
@@ -0,0 +1,35 @@
+@extends("layout")
+@section('content')
+<div class="text-center text-2xl py-7">
+    <h1>INVENTORY</h1>
+</div>
+<div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4  my-10 justify-items-center">
+    @foreach($cars as $item)
+    <div class="card w-96 bg-base-100 shadow-xl">
+        <figure class="px-10 pt-10">
+          <img src="{{$item->img_link}}" alt="Shoes" class="w-full" style="height:200px" />
+        </figure>
+        <div class="card-body ">
+            <div class="flex justify-between">
+                <div>
+                    <h2 class="card-title m-2">{{$item->model}}</h2>
+                    <p class="text-sm m-2">VIN- {{$item->vin}}</p>
+                </div>
+              <div class="m-2">
+                $-{{$item->price}}
+              </div>
+            </div>
+        
+            <div class="card-actions">
+                <button class="btn btn-primary mx-auto my-4">Details</button>
+              </div>
+        </div>
+       
+      </div>
+    @endforeach
+    
+</div>
+@endsection
+@section("title")
+Workspaceit | Car List
+@endsection
---
+++ infotect_task/resources/views/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.24.2/dist/full.css" rel="stylesheet" type="text/css" />
+     <script src="https://cdn.tailwindcss.com"></script>
+    <title>@yield("title")</title>
+</head>
+<body>
+    <header>
+        <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box"> 
+            <div class="collapse-title text-xl font-medium">
+                <h1>Workspace</h1>
+                <h1>Infotech</h1> 
+            </div>
+          </div>
+    </header>
+    <div>
+        @yield("content")
+    </div>
+
+        <footer class="footer footer-center items-center p-4 bg-neutral text-neutral-content">
+            <div class="items-center grid-flow-col ">
+              <p>Copyright © Workspaceit - All right reserved</p>
+            </div> 
+          </footer>
+</body>
+</html>
