b22da72dde155bb15370b5f7edbfa8f38145ece8
didipta <66532706+didipta@users.noreply.github.com>
2022-09-08 19:41:20 +0600

    update

---
+++ infotect_wordpress_task/wp-content/themes/car-dealer-nexcars/includes/theme-help.php
@@ -0,0 +1,41 @@
+<section class="layers-area-wrapper">
+    <div class="layers-onboard-wrapper">
+
+        <div class="layers-onboard-slider">
+            <!-- STEP 1 -->
+            <div class="layers-onboard-slide layers-animate layers-onboard-slide-current">
+                <div class="notice is-dismissible" style="margin-left:0;">
+                    <div class="layers-content-large">
+                        <!-- Your content goes here -->
+                        <div class="layers-section-title layers-small layers-no-push-bottom">
+                            <h3 class="layers-heading">
+                                <?php _e('Thanks for Installing the Car Dealer NexCars Theme!', 'car-dealer-nexcars'); ?>
+                            </h3>
+                            <div class="layers-excerpt">
+                                <p>
+                                    <?php _e('This setup wizard serves as an introduction to your theme and any special options it may have.', 'car-dealer-nexcars'); ?>
+                                </p>
+                                <ul>
+                                    <li><a href="<?php echo esc_url(get_admin_url() . "themes.php?page=tgmpa-install-plugins");?>"><?php _e('Open the Install Theme', 'car-dealer-nexcars'); ?></a></li>
+                                    <li><a href="https://demo.wpdirectorykit.com/car-dealer-nexcars/" target="_blank"><?php _e('Open the Demo Preview', 'car-dealer-nexcars'); ?></a></li>
+                                    <li><a href="https://www.wpdirectorykit.com/" target="_blank"><?php _e('Visit Our Portal', 'car-dealer-nexcars'); ?></a></li>
+                                    <li><a href="https://www.wpdirectorykit.com/theme_preview/car-dealer-nexcars" target="_blank"><?php _e('Our Themes', 'car-dealer-nexcars'); ?></a></li>
+                                    <li><a href="https://www.wpdirectorykit.com/plugins.html" target="_blank"><?php _e('Our Plugins', 'car-dealer-nexcars'); ?></a></li>
+                                    <li><a href="https://www.wpdirectorykit.com/contact.html" target="_blank"><?php _e('Support Us', 'car-dealer-nexcars'); ?></a></li>
+                                </ul>    
+                            </div>
+                        </div>
+                    </div>
+                    <div class="layers-button-well" style="margin-bottom: 15px;">
+                        <a class="layers-button btn-primary layers-pull-right onbard-next-step" href="<?php echo esc_url(get_admin_url() . "themes.php?page=tgmpa-install-plugins");?>"><?php _e('Got it, Next Step &rarr; and Install One Click Demo Import', 'car-dealer-nexcars'); ?></a>
+                    </div>
+                </div>
+                <br/>
+                <div class="layers-column layers-span-8 no-gutter layers-demo-video">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/help.jpg'; ?>" />
+                </div>
+            </div>
+            <br/>
+        </div>
+    </div>
+</section>
